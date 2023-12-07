ee608cfbe8b0774653b18a4176ef0c37138956e1
syko <49118008+elsyko@users.noreply.github.com>
2023-12-07 11:20:24 +1030

    fix/config: base path

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue({
       template: { transformAssetUrls }
